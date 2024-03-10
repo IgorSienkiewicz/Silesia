e6c0de01b00098de920081ba74b8340ddfdf5a87
LubieKakao1212 <grzesio.go@gmail.com>
2024-03-10 18:49:49 +0100

    Dialogues

--- Assets/Scripts/POI.cs
+++ Assets/Scripts/POI.cs
@@ -7,24 +7,33 @@ public class POI : MonoBehaviour
 {
     [field: SerializeField]
     public CinemachineVirtualCamera PoiCamera { get; private set; }
-
+/*
     [SerializeField] private LayerMask enabledLayers;
-    [SerializeField] private LayerMask disabledLayers;
+    [SerializeField] private LayerMask disabledLayers;*/
 
     [SerializeField] private GameObject marker;
 
     [SerializeField] private Collider targetCollider;
     [SerializeField] private POI[] nextPois;
 
+    private void Awake()
+    {
+        Disable();
+    }
+
     private void Enable()
     {
-        targetCollider.layerOverridePriority = enabledLayers.value;
+        /*targetCollider.excludeLayers = 0;
+        targetCollider.includeLayers = enabledLayers.value;*/
+        targetCollider.gameObject.layer = 6;
         marker.SetActive(true);
     }
     
     private void Disable()
     {
-        targetCollider.layerOverridePriority = disabledLayers.value;
+        //targetCollider.layerOverridePriority = disabledLayers.value;
+        targetCollider.gameObject.layer = 0;
+        //targetCollider.includeLayers = 0;
         marker.SetActive(false);
     }
 
